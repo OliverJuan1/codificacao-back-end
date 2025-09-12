28e0962354204cbe2e43530ea89621c7b2f2cec0
OliverJuan1 <Juan883462@gmail.com>
2025-09-11 21:54:57 -0300

    desenvolvimento de APIs

--- aulas/aula-18-Renan/services/taskService.js
+++ aulas/aula-18-Renan/services/taskService.js
@@ -2,7 +2,8 @@ const express = require('express')
 
 let tasks = [
     { id: 1, title: "Estudar Apis", done: false },
-    { id: 2, title: "Praticar Criação de Apis", done: true }
+    { id: 2, title: "Praticar Criação de Apis", done: true },
+    { id: 3, title: "Ler sobre Node.js", done: false }
 ]
 
 // Regras de negocio:
@@ -20,35 +21,59 @@ const getById = (id) => {
     return task
 }
 
-const update = (id, {title, done}) => {
+const update = (id, { title, done }) => {
     const task = getById(id);
 
-    if(title) {
+    if (title) {
         const exists = tasks.some(
             t => t.title.toLowerCase() === title.toLowerCase() && t.id !== id
         );
 
-        if(exists) 
+        if (exists)
             throw { status: 400, message: "Já existe outra tarefa com este titulo" };
-        
+
         task.title = title;
 
     }
 
-    if(done !== undefined) {
-        if(typeof done !== "boolean")
-            throw { status: 400, message: "O campo done deve ser 'true' ou 'false' "};
+    if (done !== undefined) {
+        if (typeof done !== "boolean")
+            throw { status: 400, message: "O campo done deve ser 'true' ou 'false' " };
 
         task.done = done;
     }
 }
 const create = (title) => {
+    if (!title)
+        throw { status: 400, message: "O campo 'títle' é obrigatório" };
 
-}
+    const exists = tasks.some(
+        t.title.toLowerCase() === title.toLowerCase()
+    );
+
+    if (exists)
+        throw { status: 400, message: "Já existe outra tarefa com este titulo" };
+
+    const newTask = { id: tasks.length + 1, title, done: false };
+    tasks.push(newTask);
+    return newTask;
+    };
+
 
 const del = (id) => {
+    const taskIndex = tasks.findIndex(t => t.id === id);
+    if (taskIndex === -1)
+        throw { status: 404, message: "Tarefa não encontrada" };
+
+    if (tasks[taskIndex].done){
+        throw{ status: 400, message: "Não é permitido excluir uma tarefa concluída" };
+    }
+
+    const deleted = tasks.splice(taskIndex, 1)[0];
+    return deleted;
     
 }
 
-module.exports = { getAll, getById }
+module.exports = { getAll, getById, create, update, del }
+
 
