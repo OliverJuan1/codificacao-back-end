f369428f80cac5e401b77103ef1ac6a9f6ab57e7
OliverJuan1 <Juan883462@gmail.com>
2025-09-11 21:37:37 -0300

    desenvolvimento de APIs

--- aulas/aula-18-Renan/services/taskService.js
+++ aulas/aula-18-Renan/services/taskService.js
@@ -1,25 +1,54 @@
-const express = require('express');
+const express = require('express')
 
 let tasks = [
     { id: 1, title: "Estudar Apis", done: false },
     { id: 2, title: "Praticar Criação de Apis", done: true }
-];
+]
 
+// Regras de negocio:
+// O título é obrigatório
+// N pode haver título duplicado
+// So é possível concluir uma tarefa se ela existir
+// N é permitido atualizar ou excluir uma tarefa inexistente
+// N é permitido excluir uma tarefa concluída
 
-// Regras de negócio
-// o titulo é obrigatório
-// não pode haver titulo duplicado
-// so e possível marcar como feita uma tarefa que exista
-// nao e permitiso atualizar  ou excluir uma tarefa inexistente
-// nao e permitido excluir uma tarefa concluida
-
-
-const getAll = () => tasks;
+const getAll = () => tasks
 
 const getById = (id) => {
-    const task = tasks.find(t => t.id === id);
-    if (!task) throw { status: 404, message: "Tarefa não encontrada" };
-    return task;
+    const task = tasks.find(t => t.id === id)
+    if (!task) throw { status: 404, message: "Tarefa não encontrada" }
+    return task
 }
 
-module.exports = { getAll, getById };
+const update = (id, {title, done}) => {
+    const task = getById(id);
+
+    if(title) {
+        const exists = tasks.some(
+            t => t.title.toLowerCase() === title.toLowerCase() && t.id !== id
+        );
+
+        if(exists) 
+            throw { status: 400, message: "Já existe outra tarefa com este titulo" };
+        
+        task.title = title;
+
+    }
+
+    if(done !== undefined) {
+        if(typeof done !== "boolean")
+            throw { status: 400, message: "O campo done deve ser 'true' ou 'false' "};
+
+        task.done = done;
+    }
+}
+const create = (title) => {
+
+}
+
+const del = (id) => {
+    
+}
+
+module.exports = { getAll, getById }
+
