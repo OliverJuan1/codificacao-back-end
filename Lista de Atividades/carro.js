cecc4a63764d7f62879d05f40962e37b28fd9954
OliverJuan1 <Juan883462@gmail.com>
2025-09-17 19:46:17 -0300

    feita a lista de atividades

--- Lista de Atividades/carro.js
+++ Lista de Atividades/carro.js
@@ -1,40 +1,55 @@
-// 18. Crie uma classe Carro com atributos marca, modelo, ano.
-// a) Adicione métodos
-// - ligar() -> retorna “Carro ligado”.
-// - desligar() retorna “Carro desligado”.
-// b) Instancie 3 carros diferentes e exiba o resultado
+class Carro {
+    constructor(marca, modelo, ano){
 
-class Carro {  
-    constructor(marca, modelo, ano) {
-        this.marca = marca;
-        this.modelo = modelo;
-        this.ano = ano;
+        this.marca = marca
+        this.modelo = modelo
+        this.ano = ano
+        
     }
+
     setMarca(marca) {
-        this.marca = marca;
+        this.marca = marca
     }
+
     setModelo(modelo) {
-        this.modelo = modelo;
-    }
-    setAno(ano) {
-        this.ano = ano;
+        this.modelo = modelo
+
     }
 
-    ligar() {
-        return "Carro ligado";
-    }
-    desligar() {
-        return "Carro desligado";
+    setAno(ano){
+        this.ano = ano
     }
 
-    getMarca() {
-        return this.marca;
+    getMarca(){
+        return this.marca
     }
-    getModelo() {
-        return this.modelo;
+
+    getModelo(){
+        return this.modelo
     }
-    getAno() {
-        return this.ano;
-    }   
+
+    getAno(){
+        return this.ano
+    }
+
+    
 }
+
+const carro1 = new Carro();
+carro1.setAno('2025');
+carro1.setModelo('Sedan');
+carro1.setMarca('Honda');
+
+carro1.getAno();
+carro1.getModelo();
+carro1.getMarca();
+
+const carro2 = new Carro();
+carro2.setAno('2026');
+carro1.setModelo('Hatch');
+carro1.setMarca('Toyota');
+
+carro2.getAno();
+carro2.getModelo();
+carro2.getMarca();
 
